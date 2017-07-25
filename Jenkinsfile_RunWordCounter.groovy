208826f4c98a60645ec0a6847f44ea97753a6aaa
Artem Tyrnov <tyrnov@protonmail.com>
2017-07-26 01:38:49 +0300

    Updated Jenkinsfile w/ notification logic

--- Jenkinsfile_RunWordCounter.groovy
+++ Jenkinsfile_RunWordCounter.groovy
@@ -20,3 +20,17 @@ pipeline {
         }
     }
 }
+
+catch (exc) {
+    echo "Caught: ${exc}"
+
+    String recipient = 'tyrnov@protonmail.com'
+
+    mail subject: "${env.JOB_NAME} (${env.BUILD_NUMBER}) failed",
+            body: "It appears that ${env.BUILD_URL} is failing, somebody should do something about that",
+              to: recipient,
+         replyTo: recipient,
+            from: 'noreply@jenkins-server.io'
+}
+
+// vim: ft=groovy
